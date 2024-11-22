aae6279c9c58585834ab4fbaa2af66785a00d880
Empester <secur2008treb@gmail.com>
2024-11-22 20:54:19 +0200

    small fix

--- redirect.js
+++ redirect.js
@@ -1,13 +1,9 @@
 
-// Get the full URL
 const fullURL = window.location.href;
 
-// Extract the file name without the extension
 const path = fullURL.substring(fullURL.lastIndexOf('/') + 1).split('.')[0];
 
-// Check if there's a file extension
 if (fullURL.includes('.html')) {
-  // Rewrite the URL without reloading the page
   const newURL = `${window.location.origin}${window.location.pathname.replace('.html', '')}`;
   window.history.replaceState(null, '', newURL);
 }
